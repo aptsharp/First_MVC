8e20f6c615fdce369a7cc70e2cb4f0b332cd5052
-aptsharp <afonso.raffael@gmail.com>
2019-11-05 15:22:03 -0300

    CRUD Scaffolding

---
+++ Data/FistAplicationMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fist.Aplication.MVC.Models
+{
+    public class FistAplicationMVCContext : DbContext
+    {
+        public FistAplicationMVCContext (DbContextOptions<FistAplicationMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Fist.Aplication.MVC.Models.Departament> Departament { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Fist.Aplication.MVC.Models;
 
 namespace Fist.Aplication.MVC
 {
@@ -24,6 +26,9 @@ namespace Fist.Aplication.MVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<FistAplicationMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("FistAplicationMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
